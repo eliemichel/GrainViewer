f1e7dd3da4c202465a2d445f061eafb2a3fdc0cc
Elie Michel <eliemichel@users.noreply.github.com>
2020-11-03 14:50:34 +0100

    Fix unzip command for linux

--- download-data.sh
+++ download-data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Download additionnal heavy data, that does not belong in a git repo
 wget https://perso.telecom-paristech.fr/emichel/share/GrainViewer-Additional-Data-v1.0.0.zip -O share/scenes/GrainViewer-Additional-Data-v1.0.0.zip
-unzip share/scenes/GrainViewer-Additional-Data-v1.0.0.zip share/scenes
+unzip share/scenes/GrainViewer-Additional-Data-v1.0.0.zip -d share/scenes
 
 # Check that it run all right
 if [ $? -eq 0 ]
