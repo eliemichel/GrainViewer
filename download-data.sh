0abd09f8fb483ce141300c798a36dd5a800151be
Elie Michel <elie.michel@exppad.com>
2020-11-03 17:09:57 +0100

    Keep on fixing building issues on linux

--- download-data.sh
+++ download-data.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # Download additionnal heavy data, that does not belong in a git repo
-wget https://perso.telecom-paristech.fr/emichel/share/GrainViewer-Additional-Data-v1.0.0.zip -O share/scenes/GrainViewer-Additional-Data-v1.0.0.zip
-unzip share/scenes/GrainViewer-Additional-Data-v1.0.0.zip -d share/scenes
+ZIPFILE=share/scenes/GrainViewer-Additional-Data-v1.0.0.zip
+if [ ! -f "$ZIPFILE" ]; then
+	wget https://perso.telecom-paristech.fr/emichel/share/GrainViewer-Additional-Data-v1.0.0.zip -O $ZIPFILE
+fi
+unzip $ZIPFILE -d share/scenes
 
 # Check that it run all right
 if [ $? -eq 0 ]
